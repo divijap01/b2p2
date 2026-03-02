7af40496c0b2213915691f7ecc4ed5a491468d1b
divijap01 <divijavikaspawar01@gmail.com>
2026-03-03 02:09:35 +0530

    UC9:Recursive palindrome checker

--- src/Main.java
+++ src/Main.java
@@ -1,119 +1,46 @@
-@@ -1,35 +1,97 @@
-        import java.util.*;
-
-class Node {
-    char data;
-    Node next;
-
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
+@@ -68,30 +68,21 @@ static Node reverse(Node head) {
+    current = nextNode;
 }
-
-public class PalindromeCheckerApp {
-
-    static Node createList(String str) {
-        Node head = null, tail = null;
-
-        for (char ch : str.toCharArray()) {
-            Node newNode = new Node(ch);
-
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-        return head;
-    }
-
-    static boolean isPalindrome(Node head) {
-        if (head == null || head.next == null)
-            return true;
-
-        Node slow = head;
-        Node fast = head;
-
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data)
-                return false;
-
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
-    }
-
-    static Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
-
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
         return prev;
     }
+static boolean palidrome(String s , int start , int end){
+    if(start >= end) return  true;
+    if(s.charAt(start)!=s.charAt(end)) return false;
+    return palidrome(s,start+1,end -1);
+}
 
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Input text : ");
+    String input = sc.nextLine();
+    Deque<Character> dq = new ArrayDeque<>();
+    boolean palindrome = true;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input text : ");
         String input = sc.nextLine();
-        Stack<Character> st = new Stack<>();
-        Queue<Character> q = new LinkedList<>();
-        Deque<Character> dq = new ArrayDeque<>();
-        boolean palindrome = true;
 
         for(char c : input.toCharArray()){
-            q.add(c);
-            st.push(c);
             dq.addLast(c);
         }
-
-        boolean palindrome = true;
-        while(q.size()>0){
-            if(q.poll() != st.pop()){
-                palindrome = false;
-                break;
-            }
-            while(dq.size() > 1){
-                char first = dq.removeFirst();
-                char second = dq.removeLast();
-                if(first != second) palindrome = false;
-                break;
-            }
-            System.out.println("Is Palindrom? :" +palindrome);
-
-            System.out.println("Is Palindrome? :" + palindrome);
-            while(dq.size() > 1){
-                char first = dq.removeFirst();
-                char second = dq.removeLast();
-                if(first != second) palindrome = false;
-                break;
-            }
+        while(dq.size() > 1){
+            char first = dq.removeFirst();
+            char second = dq.removeLast();
+            if(first != second) palindrome = false;
+            break;
         }
         System.out.println("Is Palindrome? :" + palindrome);
     }
 }
 Node head = createList(input);
         System.out.println("Is Palindrom? :"+isPalindrome(head));
+Node head = createList(input);
+            System.out.println("Is Palindrom? :"+isPalindrome(head));
 
         sc.close();
     }
             }
+            System.out.println("Is Palindrom? :"+ palidrome(input,0,input.length()-1));
+
+        sc.close();
+        }
